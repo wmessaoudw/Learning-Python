e96b9610129d8a3bc8f5190428224ecab16ada3f
Messaoud <wmessaoudw@gmail.com>
2026-09-10 21:48:19 +0100

    First Day Of Python Lesson

---
+++ day1.py
@@ -0,0 +1,17 @@
+#displaying in terminal
+print("Hello,World")
+#Operations
+print(3+2) #additon
+print(3-2) #subtraction
+print(3*2) #Multpilication
+print(3/2) #division
+print(3%2) #Modulus
+print(3**2) #exponent
+print(3//2) #Floor Division
+
+
+#Checking Types Use The function type()
+#ex
+print(type(3)) #int
+print(type(3.4)) #float
+#ect ........
